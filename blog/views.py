480e98f65fc0e3fda74595c38b9e17b0eca6ca26
Asumanz <asugulob@gmail.com>
2016-10-25 12:19:44 +0300

    Completed tutorial for djangogirls

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect
+from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
@@ -36,7 +37,7 @@ def post_edit(request, pk):
 			post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
-		else:
-			form = PostForm(instance=post)
-		return render(request, 'blog/post_edit.html', {'form': form})
-		
+	else:
+		form = PostForm(instance=post)
+	return render(request, 'blog/post_edit.html', {'form': form})
+	
